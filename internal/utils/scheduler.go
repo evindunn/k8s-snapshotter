c5c39d4106a17f2635d149cdab064da30608fbb5
Evin Dunn <evindunn14@gmail.com>
2021-07-05 11:26:50 -0700

    Comments for scheduler

--- internal/utils/scheduler.go
+++ internal/utils/scheduler.go
@@ -6,6 +6,10 @@ import (
 	"sync"
 )
 
+/*
+BackupJobInput is the input required to run an asynchronous backup of a given PVCName in a given
+Namespace
+ */
 type BackupJobInput struct {
 	ClientSet   *kubernetes.Clientset
 	CSIClient   *csiV1.Clientset
@@ -13,8 +17,16 @@ type BackupJobInput struct {
 	PVCName		string
 }
 
+/*
+BackupJobRunner is the function that is called on each BackupJobInput in order to perform the
+backup
+ */
 type BackupJobRunner func(wg *sync.WaitGroup, output chan <- error, input *BackupJobInput)
 
+/*
+BackupJobScheduler is responsible for running BackupJobRunner on multiple BackupJobInput instances in
+parallel
+ */
 type BackupJobScheduler struct {
 	jobFunc       BackupJobRunner
 	waitGroup     sync.WaitGroup
@@ -22,6 +34,10 @@ type BackupJobScheduler struct {
 	outputChannel chan error
 }
 
+/*
+NewBackupJobScheduler creates a new BackupJobScheduler that will call the given BackupJobRunner on multiple
+BackupJobInput instances in parallel
+ */
 func NewBackupJobScheduler(job BackupJobRunner) *BackupJobScheduler  {
 	scheduler := BackupJobScheduler{
 		jobFunc:       job,
@@ -33,10 +49,17 @@ func NewBackupJobScheduler(job BackupJobRunner) *BackupJobScheduler  {
 	return &scheduler
 }
 
+/*
+Schedule submits the given BackupJobInput to the scheduler's inputChannel
+ */
 func (scheduler *BackupJobScheduler) Schedule(input *BackupJobInput)  {
 	scheduler.inputChannel <- input
 }
 
+/*
+Run watches the scheduler's inputChannel, spawning a goroutine running the BackupJobRunner for each
+BackupJobInput submitted to the channel
+ */
 func (scheduler *BackupJobScheduler) Run() {
 	defer close(scheduler.outputChannel)
 	for input := range scheduler.inputChannel {
@@ -46,6 +69,10 @@ func (scheduler *BackupJobScheduler) Run() {
 	scheduler.waitGroup.Wait()
 }
 
+/*
+Wait closes the scheduler's inputChannel. This triggers the end of the loop in Run, which in turn
+closes the scheduler's outputChannel. The outputChannel is then returned so it can be read from.
+ */
 func (scheduler *BackupJobScheduler) Wait() chan error {
 	close(scheduler.inputChannel)
 	return scheduler.outputChannel
